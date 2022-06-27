a53e46c3114d5336879da5edc3acaa3269d8c5f0
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-06-27 18:09:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const router = require('./routes');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 const limiter = rateLimit({
